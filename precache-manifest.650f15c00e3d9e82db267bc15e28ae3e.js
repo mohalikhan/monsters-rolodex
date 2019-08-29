a744ff1ffe4ddcf8d558e4c5f23a75e00b8057de
Mohammad Ali <mohalikhan@hotmail.com>
2019-08-29 12:46:28 -0400

    Updates

---
+++ precache-manifest.650f15c00e3d9e82db267bc15e28ae3e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a53a2e63402da519f34704680b5cad9",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "a52b4073fbf8974d7f02",
+    "url": "/monsters-rolodex/static/css/main.6f88b6bd.chunk.css"
+  },
+  {
+    "revision": "d81979aa8ac68af22713",
+    "url": "/monsters-rolodex/static/js/2.4ea1ec15.chunk.js"
+  },
+  {
+    "revision": "a52b4073fbf8974d7f02",
+    "url": "/monsters-rolodex/static/js/main.27ddcdca.chunk.js"
+  },
+  {
+    "revision": "c7f991e6cb4746d6bd30",
+    "url": "/monsters-rolodex/static/js/runtime~main.205b164d.js"
+  }
+]);
